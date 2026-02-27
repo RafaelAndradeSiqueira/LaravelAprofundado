77fb7e7160b498b73a68e4c953af61e1e83f4e20
Rafael Andrade <r-andrade-siqueira@hotmail.com>
2026-02-27 13:18:26 -0300

    Alteracao de rota

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Cache;
 
 Route::get('/', function () {
-    Cache::put('nome', 'Rafael Siqueira', now()->addMinutes(10));
-
-    dd([
-        'store' => config('cache.default'),
-        'value' => Cache::get('nome'),
-    ]);
-});
+    return view('welcome');
+    }
+);
